371412206f307c46675b181f5eebf96d4d95b654
Yiyu Qian <yiyuqianunitediting@gmail.com>
2024-06-06 00:05:54 +0800

    update view and urls

--- mysite/api/urls.py
+++ mysite/api/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("blogposts/", views.BlogPostListCreate.as_view(), name="blogpost-view-create"),
-    path("blogposts/<int:pk>/", views.BlogPostReteriveUpdateDestory.as_view(), name="update")
+    path("blogposts/<int:pk>/", views.BlogPostReteriveUpdateDestory.as_view(), name="update"),
+    path("blogposts/all/", views.BlogPostList.as_view(), name="blogpost-view-all")
 ]
